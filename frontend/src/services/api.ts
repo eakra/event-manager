e2032802067bf6d163f4457eccabb31af63c3e28
eakravtsov <eakravtsov@gmail.com>
2026-03-27 15:31:09 +0000

    mm

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = window.location.hostname === 'localhost' 
+  ? 'http://localhost:8080/api' 
+  : '/api';
 
 const api = axios.create({
   baseURL: API_BASE,
